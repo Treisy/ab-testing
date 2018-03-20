ad7351d2630a4fdf101d4f9df1ebb8b425f453a4
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2018-03-20 09:50:28 -0600

    Apply design for variation #1

--- Exp24 - Registration Funnel CTR/exp24b.js
+++ Exp24 - Registration Funnel CTR/exp24b.js
@@ -1,6 +1,5 @@
 var btnSuscribeTemplate = '<li><a class="btn btn-fill btn-subscribe" aria-labelledby="Subscribe" href="#buttons"></a></li>';
 
-var iconsContainer = document.querySelector('.sticky-share figure ul');
 
 function changeText() {
     var stickyTitle = document.getElementsByClassName('sticky-share-title')[0];
@@ -10,28 +9,25 @@ function changeText() {
 
 
 function removeIcons() {
-   $('.sticky-share figure ul').remove('li');
+    $('.sticky-share figure ul').remove('li');
 }
 
-function addIconsWithText(){
-    var iconDownload = '<li><a href="/~/media/McKinsey/Business Functions/Organization/Our Insights/The agenda of a talen first CEO/An-agenda-for-the-talent-first-CEO.ashx" data-capture-key="download-this-article" rel="nofollow" data-show-popup="login-overlay" class="mck-download-icon social-contact" aria-labelledby="open-interactive" target="_blank">Download<span class="visually-hidden">Download this article</span> </a></li>';
+function addOptions(){
+    var iconEmail = '<li><a data-capture-key="email" data-show-popup="login-overlay" class="mck-email-icon social-contact" aria-labelledby="email-interactive" target="_blank"></a></li>';
 
-    var iconPrint = '<li class="print"><a data-capture-key="print-this-article" data-show-popup="login-overlay" class="mck-print-icon social-contact" aria-labelledby="open-interactive" target="_blank">Print</a></li>';
+    var iconShare = '<li><a data-capture-key="share-this-article" data-show-popup="login-overlay" class="mck-share-icon social-contact" aria-labelledby="share-interactive" target="_blank"></a></li>';
 
-    var iconShare = '<li><a data-capture-key="share-this-article" data-show-popup="login-overlay" class="mck-share-icon social-contact" aria-labelledby="share-interactive" target="_blank">Share</a></li>';
+    var optionsArray = [];
 
-    var iconsArray = [];
-
-    iconsArray.push(iconDownload);
-    iconsArray.push(iconPrint);
-    iconsArray.push(iconShare);
-    iconsArray.push(btnSuscribeTemplate);
+    optionsArray.push(iconEmail);
+    optionsArray.push(iconShare);
+    optionsArray.push(btnSuscribeTemplate);
     
-    $('.sticky-share figure ul').html(iconsArray.join(''));
+    $('.sticky-share figure ul').html(optionsArray.join(''));
 }
 
-$(document).ready(function() {
+$(document).ready(function(){
     changeText();
     removeIcons();
-    addIconsWithText();
+    addOptions();
 });
