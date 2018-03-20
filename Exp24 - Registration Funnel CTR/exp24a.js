224de7ceb862cb047ac5f3c49058bc65f1673c88
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2018-03-20 08:39:38 -0600

    Apply new design for variation #0

--- Exp24 - Registration Funnel CTR/exp24a.js
+++ Exp24 - Registration Funnel CTR/exp24a.js
@@ -1,5 +1,4 @@
-var btnSuscribeTemplate = '<li class="subscribe"><a class="btn btn-fill btn-subscribe" aria-labelledby="Subscribe" href="#buttons"></a></li>';
-var iconsContainer = document.querySelector('.sticky-share figure ul');
+var btnSubscribeTemplate = '<li class="subscribe"><a class="btn btn-fill btn-subscribe" aria-labelledby="Subscribe" href="#buttons"></a></li>';
 
 function changeText() {
     var stickyTitle = document.getElementsByClassName('sticky-share-title')[0];
@@ -8,38 +7,23 @@ function changeText() {
 }
 
 function removeIcons() {
-    var iconLinkedIn = document.querySelector('.sticky-share figure ul li a.mck-linkedin-icon');
-    var iconTwitter = document.querySelector('.sticky-share figure ul li a.mck-twitter-icon');
-    var iconFacebook = document.querySelector('.sticky-share figure ul li a.mck-facebook-icon');
-    
-    iconLinkedIn.parentNode.removeChild(iconLinkedIn);
-    iconTwitter.parentNode.removeChild(iconTwitter);
-    iconFacebook.parentNode.removeChild(iconFacebook);
+    $('.sticky-share figure ul').remove('li');
+ }
+
+function addOptions() {
+    var optionsArray = [];
+
+    var iconShare = '<li><a data-capture-key="share-this-article" data-show-popup="login-overlay" class="mck-share-icon social-contact" aria-labelledby="share-interactive" target="_blank">Share</a></li>';
+
+    optionsArray.push(iconShare);
+    optionsArray.push(btnSubscribeTemplate);
+
+    $('.sticky-share figure ul').html(optionsArray.join(''));
+
 }
 
-function addIcon(){
-    var iconShare = '<li><a data-capture-key="share-this-article" data-show-popup="login-overlay" class="interactive-link show-popup mck-share-icon download-this-article" aria-labelledby="share-interactive" role="button" target="_blank"></a></li>';
-    
-    var iconsArray = [];
-
-    var download = document.querySelector('.sticky-share figure ul li .mck-download-icon').parentElement;
-
-    var print = document.querySelector('.sticky-share figure ul li .mck-print-icon').parentElement;
-
-    var iconPrint = '<li>' + print.innerHTML + '</li>';
-    var iconDownload = '<li>' + download.innerHTML + '</li>';
-
-    iconsArray.push(btnSuscribeTemplate);
-    iconsArray.push(iconDownload);
-    iconsArray.push(iconPrint);
-    iconsArray.push(iconShare);
-
-    
-    $('.sticky-share figure ul').html(iconsArray.join(''));
-}
-
-$(document).ready(function(){
+ $(document).ready(function() {
     changeText();
     removeIcons();
-    addIcon();
-});
+    addOptions();
+ });
