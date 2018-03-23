575706ee0867596825d4aea5cdcda5f69cec46a5
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2018-03-23 09:05:30 -0600

    Add sticky banner footer

--- Exp24 - Registration Funnel CTR/exp24a.js
+++ Exp24 - Registration Funnel CTR/exp24a.js
@@ -1,4 +1,4 @@
-var btnSubscribeTemplate = '<li class="subscribe"><a class="btn btn-fill btn-subscribe" aria-labelledby="Subscribe" href="#buttons"></a></li>';
+
 
 function changeText() {
     $('.sticky-share-title').text('Making it in America: Revitalizing US manufactoring');
@@ -11,6 +11,8 @@ function removeIcons() {
 function addOptions() {
     var optionsArray = [];
 
+    var btnSubscribeTemplate = '<li class="subscribe"><a class="btn btn-fill btn-subscribe" aria-labelledby="Subscribe" href="#buttons"></a></li>';
+
     var iconShare = '<li><a data-capture-key="share-this-article" data-show-popup="login-overlay" class="mck-share-icon social-contact" aria-labelledby="share-interactive" target="_blank">Share</a></li>';
 
     optionsArray.push(iconShare);
@@ -19,8 +21,48 @@ function addOptions() {
     $('.sticky-share figure ul').html(optionsArray.join(''));
 }
 
+function stickyBannerTemplate() {
+    var stickyTemplate = '<div class="sticky-banner-container">' +
+                            '<div class="sticky-banner">' +
+                                ' <div class="sticky-banner-outer-wrapper">'+
+                                    '<div class="sticky-banner-wrapper"></div>'+
+                                    '</div>'+
+                                '</div>'+
+                        '</div>';
+
+    var stickyTitle = '<span class="sticky-banner-title">Stay current on your favorite topics</span>';
+
+    var btnSubscribe = '<a class="btn btn-fill btn-subscribe" aria-labelledby="Subscribe" href="#buttons"></a>';
+
+    var stickyCTA = '<a href="" class="cta">No, thank you</a>';
+
+
+    $('.article-body-wrapper').append(stickyTemplate);
+    $('.sticky-banner-wrapper').append(stickyTitle);
+    $('.sticky-banner-wrapper').append(btnSubscribe);
+    $('.sticky-banner-wrapper').append(stickyCTA);
+}
+
 $(document).ready(function() {
     changeText();
     removeIcons();
     addOptions();
+    stickyBannerTemplate();
+    
+    $(window).scroll(function() {
+        var showSticky = 3000;
+        var hideSticky = 13200;
+
+        if(window.scrollY >= showSticky) {
+            $('.sticky-banner').addClass('show');
+        }
+
+        if(window.scrollY < showSticky) {
+            $('.sticky-banner').removeClass('show');
+        }
+
+        if(window.scrollY >= hideSticky) {
+            $('.sticky-banner').removeClass('show');
+        }
+    });
 });
