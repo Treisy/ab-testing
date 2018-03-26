06ca0a5cbac54231cf931354865739b6b6246051
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2018-03-26 11:30:11 -0600

    Apply validations for variation #0

--- Exp24 - Registration Funnel CTR/exp24a.js
+++ Exp24 - Registration Funnel CTR/exp24a.js
@@ -1,10 +1,13 @@
+var showSticky = 3000;
+var hideSticky = 13200;
+
 function changeText() {
     $('.sticky-share-title').text('Making it in America: Revitalizing US manufactoring');
 }
 
 function removeIcons() {
-    $('.sticky-share figure ul').remove('li');
- }
+    $('.sticky-share-tools._show').find('.sticky-share figure ul').remove('li');
+}
 
 function addOptions() {
     var optionsArray = [];
@@ -16,7 +19,7 @@ function addOptions() {
     optionsArray.push(iconShare);
     optionsArray.push(btnSubscribeTemplate);
 
-    $('.sticky-share figure ul').html(optionsArray.join(''));
+    $('.sticky-share-tools._show').find('.sticky-share figure ul').html(optionsArray.join(''));
 }
 
 function stickyBannerTemplate() {
@@ -24,41 +27,44 @@ function stickyBannerTemplate() {
                             '<div class="sticky-banner">' +
                                 ' <div class="sticky-banner-outer-wrapper">'+
                                     '<div class="sticky-banner-wrapper"></div>'+
-                                    '</div>'+
                                 '</div>'+
+                            '</div>'+
                         '</div>';
 
     var stickyTitle = '<span class="sticky-banner-title">Stay current on your favorite topics</span>';
 
     var btnSubscribe = '<a class="btn btn-fill btn-subscribe" aria-labelledby="Subscribe" href="#buttons"></a>';
 
-    var stickyCTA = '<a href="#" class="cta">No, thank you</a>';
+    var stickyCTA = '<a href="javascript:void(0);" class="cta" name="cta">No, thank you</a>';
 
 
     $('.article-body-wrapper').append(stickyTemplate);
     $('.sticky-banner-wrapper').append(stickyTitle);
     $('.sticky-banner-wrapper').append(btnSubscribe);
     $('.sticky-banner-wrapper').append(stickyCTA);
+
+    $(document).on('click', '.sticky-banner-wrapper a.cta', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        hideStickyBanner();
+        $('.sticky-banner-container').css('display', 'none');
+        return false;
+    });
 }
 
 function hideStickyBanner() {
     $('.sticky-banner').removeClass('show');
 }
 
-$('.sticky-banner-wrapper .cta').click(function(event){
-    event.preventDefault();
-    hideStickyBanner();
-});
-
 $(document).ready(function() {
-    changeText();
-    removeIcons();
-    addOptions();
     stickyBannerTemplate();
     
     $(window).scroll(function() {
-        var showSticky = 3000;
-        var hideSticky = 13200;
+        if ($('.sticky-share-tools').hasClass('_show')) {
+            changeText();
+            removeIcons();
+            addOptions();
+        }
 
         if(window.scrollY >= showSticky) {
             $('.sticky-banner').addClass('show');
