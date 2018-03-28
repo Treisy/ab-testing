49846bdee67b1c15fcb4e6a99d5db5676632430c
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2018-03-28 07:26:31 -0600

    Solved scroll issue

--- Exp24 - Registration Funnel CTR/variation #2/exp24c.js
+++ Exp24 - Registration Funnel CTR/variation #2/exp24c.js
@@ -1,23 +1,7 @@
 var showSticky = 3000;
-var timer;
 
-function addIconsWithText(){
-    var btnSubscribeTemplate = '<li class="new-icon"><a class="btn btn-fill btn-subscribe" aria-labelledby="Subscribe" href="#buttons"></a></li>';
-
-    var iconDownload = '<li class="new-icon"><a href="/~/media/McKinsey/Business Functions/Organization/Our Insights/The agenda of a talen first CEO/An-agenda-for-the-talent-first-CEO.ashx" data-capture-key="download-this-article" rel="nofollow" data-show-popup="login-overlay" class="mck-download-icon social-contact" aria-labelledby="open-interactive" target="_blank">Download<span class="visually-hidden">Download this article</span> </a></li>';
-
-    var iconPrint = '<li class="print new-icon"><a data-capture-key="print-this-article" data-show-popup="login-overlay" class="mck-print-icon social-contact" aria-labelledby="open-interactive" target="_blank">Print</a></li>';
-
-    var iconShare = '<li class="new-icon"><a data-capture-key="share-this-article" data-show-popup="login-overlay" class="mck-share-icon social-contact" aria-labelledby="share-interactive" target="_blank">Share</a></li>';
-
-    var iconsArray = [];
-
-    iconsArray.push(iconDownload);
-    iconsArray.push(iconPrint);
-    iconsArray.push(iconShare);
-    iconsArray.push(btnSubscribeTemplate);
-    
-    $('.sticky-share-tools._show').find('.sticky-share figure ul').append(iconsArray.join(''));
+function changeText() {
+    $('.sticky-share-title').text('Making it in America: Revitalizing US manufactoring');
 }
 
 function stickyBannerTemplate() {
@@ -61,21 +45,20 @@ var visibleY = function(el){
 
 $(document).ready(function() {
     stickyBannerTemplate();
-    addIconsWithText();
 
     $(window).scroll(function() {
-        if ($('.sticky-share-tools').hasClass('_show')) {
-            $('.sticky-share-title').text('Making it in America: Revitalizing US manufactoring');
-            $('.sticky-share-tools._show').find('.sticky-share figure ul li').addClass('hidden');
-        } else {
-            console.log('no show');
-            $('.sticky-share-tools').find('.sticky-share figure ul li').removeClass('hidden');
-            $('.sticky-share-tools').find('.sticky-share figure ul').remove('li.new-icon');
-        }
 
-        clearTimeout(timer);
+        setTimeout(function() {
+            if ($('.sticky-share-tools').hasClass('_show')) {
+                changeText();
+                $('.sticky-share-wrapper .sticky-share ul.new-icons').removeClass('hidden');
+                $('.sticky-share-wrapper .sticky-share ul').first().addClass('hidden');
+            } else {
+                $('.sticky-share-wrapper .sticky-share ul').first().removeClass('hidden');
+                $('.sticky-share-wrapper .sticky-share ul.new-icons').addClass('hidden');
 
-        timer = setTimeout(function() {
+            }
+
             if(window.scrollY >= showSticky && !visibleY( document.querySelector('#main_0_articleShare2_articleActions figure ul') )) {
                 $('.sticky-banner-container').addClass('fixed show');
             }
