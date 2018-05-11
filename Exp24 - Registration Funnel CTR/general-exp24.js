f7780a0834315666cbf8220ba74ff0d5d3b1cf04
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2018-05-11 11:07:31 -0600

    Apply feedback

--- Exp24 - Registration Funnel CTR/general-exp24.js
+++ Exp24 - Registration Funnel CTR/general-exp24.js
@@ -1,8 +1,17 @@
-window.authorsSpacing = function() {
+window.authorsSpacing = function () {
     var aside = document.querySelector('aside.-signup-promo');
     var footer = document.querySelector('footer.about-authors');
 
-    if ( $(aside).css('display') == 'none' ) {
+    if ($(aside).css('display') == 'none') {
         footer.classList.remove('-signupAboveAboutAuthor');
     }
+};
+
+window.hideDisruptor = function () {
+    var aside = document.querySelector('aside.disruptor.-left');
+
+    if (aside) {
+        $(aside).css('display', 'none');
+    }
+
 };
